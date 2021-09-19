494bba211a082b272921d8bc3c720c4c1444ca13
Itay Bittan <itay.bittan@gmail.com>
2021-09-19 23:28:28 +0300

    support variables in params (client for instance)

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,18 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { MyDataSourceOptions, MyQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: MyQuery) {
+    const templateSrv = getTemplateSrv();
+    return {
+      ...query,
+      bucket: query.bucket ? templateSrv.replace(query.bucket) : '',
+      prefix: query.prefix ? templateSrv.replace(query.prefix) : '',
+    };
   }
 }
