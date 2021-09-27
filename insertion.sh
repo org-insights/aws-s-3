546ecde3d1589690c09ce09b3e3fac84eaa2dcb2
Itay Bittan <itay.bittan@gmail.com>
2021-09-27 15:53:08 +0300

    adding 2 level example

--- insertion.sh
+++ insertion.sh
@@ -8,7 +8,17 @@ mc mb s3/"${BUCKET}" --ignore-existing;
 
 for day in 01 02 03 04 05 06 07 08 09 10
 do
-  echo "$day" > /temp_file.txt
-  mc cp /temp_file.txt s3/"${BUCKET}"/client=1000/date=2021-09-$day/temp_file.txt
-  rm /temp_file.txt
+  dd if=/dev/zero of=temp_file.data  bs=${day}K  count=1
+  mc cp /temp_file.data s3/"${BUCKET}"/client=1000/date=2021-09-${day}/temp_file.data
+  rm /temp_file.data
+done
+
+for day in 01 02 03 04 05 
+do
+  for hour in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+  do
+    dd if=/dev/zero of=temp_file.data  bs=1K  count=1
+    mc cp /temp_file.data s3/"${BUCKET}"/client=2000/date=2021-09-${day}/hour=${hour}/temp_file.data
+    rm /temp_file.data
+  done
 done
