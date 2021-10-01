1a3e5bd5bfe78b16d4fae2c4b6beaab7ccbe8a74
Itay Bittan <itay.bittan@gmail.com>
2021-10-01 12:42:10 +0300

    exclude DarwinARM64 from backend builds

--- Magefile.go
+++ Magefile.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
 )
 
 // Hello prints a message (shows that you can define custom Mage targets).
@@ -13,5 +14,10 @@ func Hello() {
 	fmt.Println("hello plugin developer!")
 }
 
+func BuildAllExceptDarwinARM64() { //revive:disable-line
+	b := build.Build{}
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.LinuxARM64, b.LinuxARM)
+}
+
 // Default configures the default target.
-var Default = build.BuildAll
+var Default = BuildAllExceptDarwinARM64
